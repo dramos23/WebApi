d0c07054e7e2c2f630e890cedeb2556af10ffa11
Daniel Ramos <daniel.r.23@gmail.com>
2018-12-01 20:54:20 -0300

    funcionando método get de recetas con filtro en recetascontroller

--- CookItWebApi/CookItWebApi/Controllers/RecetasController.cs
+++ CookItWebApi/CookItWebApi/Controllers/RecetasController.cs
@@ -52,6 +52,38 @@ namespace CookItWebApi.Controllers
 
         }
 
+        [HttpGet]
+        [Route("RecetasFiltro")]
+        public IActionResult ObtenerRecetasFiltro([FromBody] Dictionary<string, string> obj)
+        {
+
+            var _Recetas = _Context.Recetas.ToList();
+            _Recetas = (obj.ContainsKey("AptoCeliaco")) ? _Recetas.FindAll(x => x._AptoCeliacos == Boolean.Parse(obj["AptoCeliaco"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("AptoDiabetico")) ? _Recetas.FindAll(x => x._AptoDiabeticos == Boolean.Parse(obj["AptoDiabetico"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("AptoVegano")) ? _Recetas.FindAll(x => x._AptoVeganos == Boolean.Parse(obj["AptoVegano"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("AptoVegetariano")) ? _Recetas.FindAll(x => x._AptoVegetarianos == Boolean.Parse(obj["AptoVegetariano"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("MomentoDia")) ? _Recetas.FindAll(x => x._MomentoDia == (Receta.MomentoDia)Convert.ToInt64(obj["MomentoDia"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("PuntajeTotal")) ? _Recetas.FindAll(x => x._PuntajeTotal == Convert.ToDouble(obj["PuntajeTotal"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("Estacion")) ? _Recetas.FindAll(x => x._Estacion == (Ingrediente.Estacion)Convert.ToInt64(obj["Estacion"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("Dificultad")) ? _Recetas.FindAll(x => x._Dificultad == Convert.ToInt64(obj["Dificultad"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("CostoMayorIgual")) ? _Recetas.FindAll(x => x._Costo >= Convert.ToInt64(obj["CostoMayorIgual"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("CostoMenorIgual")) ? _Recetas.FindAll(x => x._Costo <= Convert.ToInt64(obj["CostoMenorIgual"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("CantPlatosMayorIgual")) ? _Recetas.FindAll(x => x._CantPlatos >= Convert.ToInt64(obj["CantPlatosMayorIgual"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("CantPlatosMenorIgual")) ? _Recetas.FindAll(x => x._CantPlatos <= Convert.ToInt64(obj["CantPlatosMenorIgual"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("TiempoPreparacionMayorIgual")) ? _Recetas.FindAll(x => x._TiempoPreparacion >= Convert.ToInt64(obj["TiempoPreparacionMayorIgual"])) : _Recetas;
+            _Recetas = (obj.ContainsKey("TiempoPreparacionMenorIgual")) ? _Recetas.FindAll(x => x._TiempoPreparacion <= Convert.ToInt64(obj["TiempoPreparacionMenorIgual"])) : _Recetas;
+
+
+            if (_Recetas == null)
+            {
+
+                return NotFound();
+            }
+
+            return Ok(_Recetas);
+
+        }
+
         [HttpPost]
         public IActionResult Post([FromBody] Receta _Receta)
         {
