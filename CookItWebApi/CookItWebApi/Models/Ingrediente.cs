6963c0a044f6a6ce3d7477ce388cd3a0791bd8d2
Daniel Ramos <DR176241@fi365.ort.edu.uy>
2018-12-20 14:05:44 -0300

    Modificación de Tipo a TipoIngrediente, en Ingrediente

--- CookItWebApi/CookItWebApi/Models/Ingrediente.cs
+++ CookItWebApi/CookItWebApi/Models/Ingrediente.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace CookItWebApi.Models
 {
@@ -32,8 +33,7 @@ namespace CookItWebApi.Models
             Primavera = 4,
             Varios = 5
         }
-
-        //1 = mg, 2 = ml
+       
         public enum TipoMedida {
             ml = 1,
             gr = 2
@@ -70,127 +70,10 @@ namespace CookItWebApi.Models
         [Required]
         public Estacion _Estacion { set; get; }
         [Required]
-        public Tipo _Tipo { set; get; }
-
-        //public Ingrediente(int id, string nombre, int costo, int tipoMedida, int medidaProm, int medidaGramo, int caloriasMedida, 
-        //    bool aptoCel, bool aptoDia, bool aptoVegetariano, bool aptoVegano, int idTipo, int idEstacion) {
-        //    _Id = id;
-        //    _Nombre = nombre;
-        //    _Costo = costo;
-        //    _Medida = (TipoMedida) tipoMedida;
-        //    _Estacion = (Estacion) idEstacion;
-        //    _Tipo = (Tipo) idTipo;
-        //    _MedidaPromedio = medidaProm;
-        //    _MedidaPorGramo = medidaGramo;
-        //    _CantCaloriasPorMedida = caloriasMedida;
-        //    _AptoCeliacos = aptoCel;
-        //    _AptoDiabeticos = aptoDia;
-        //    _AptoVegetarianos = aptoVegetariano;
-        //    _AptoVeganos = _AptoVeganos;
-        //}
-
-        //public Ingrediente(string nombre, int costo, int tipoMedida, int medidaProm, int medidaGramo, int caloriasMedida,
-        //    bool aptoCel, bool aptoDia, bool aptoVegetariano, bool aptoVegano, int idTipo, int idEstacion){
-        //    _Nombre = nombre;
-        //    _Costo = costo;
-        //    _Medida = (TipoMedida)tipoMedida;
-        //    _Estacion = (Estacion)idEstacion;
-        //    _Tipo = (Tipo)idTipo;
-        //    _MedidaPromedio = medidaProm;
-        //    _MedidaPorGramo = medidaGramo;
-        //    _CantCaloriasPorMedida = caloriasMedida;
-        //    _AptoCeliacos = aptoCel;
-        //    _AptoDiabeticos = aptoDia;
-        //    _AptoVegetarianos = aptoVegetariano;
-        //    _AptoVeganos = _AptoVeganos;
-        //}
-
-        //Testeado
-        //public bool Insertar()
-        //{
-        //    SqlConnection cn = ManejadorConexion.CrearConexion();
-
-        //    SqlCommand cmd = new SqlCommand(@"INSERT INTO Ingredientes VALUES (@Nombre, @Costo, @Medida, @MedidaPromedio, @MedidaPorGramo, 
-        //                                     @CantCalorias, @AptoCeliacos, @AptoDiabeticos, @AptoVegetarianos, @AptoVeganos, @Tipo, @Estacion); 
-        //                                     SELECT SCOPE_IDENTITY()", cn);
-        //    cmd.Parameters.Add(new SqlParameter("@Nombre", _Nombre));
-        //    cmd.Parameters.Add(new SqlParameter("@Costo", _Costo));
-        //    cmd.Parameters.Add(new SqlParameter("@Medida", _Medida.ToString()));
-        //    cmd.Parameters.Add(new SqlParameter("@MedidaPromedio", _MedidaPromedio));
-        //    cmd.Parameters.Add(new SqlParameter("@MedidaPorGramo", _MedidaPorGramo));
-        //    cmd.Parameters.Add(new SqlParameter("@CantCalorias", _CantCaloriasPorMedida));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoCeliacos", (bool) _AptoCeliacos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoDiabeticos", (bool)_AptoDiabeticos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoVegetarianos", (bool)_AptoVegetarianos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoVeganos", (bool) _AptoVeganos));
-        //    cmd.Parameters.Add(new SqlParameter("@Tipo", (int) _Tipo));
-        //    cmd.Parameters.Add(new SqlParameter("@Estacion", (int) _Estacion));
-        //    try
-        //    {
-        //        ManejadorConexion.AbrirConexion(cn);
-        //        this._Id = Convert.ToInt32(cmd.ExecuteScalar());
-        //        return true;
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        System.Diagnostics.Debug.Assert(false, ex.Message);
-        //        return false;
-
-        //    }
-        //    finally
-        //    {
-        //        ManejadorConexion.CerrarConexion(cn);
-        //    }
-        //}
-        ////Testeado
-        //public bool Actualizar()
-        //{
-        //    SqlConnection cn = ManejadorConexion.CrearConexion();
-
-        //    SqlCommand cmd = new SqlCommand();
-        //    cmd.CommandText = @"UPDATE Ingredientes 
-        //                        SET nombre = @Nombre, costo = @Costo, medida = @Medida, medidaPromedio = @MedidaPromedio, 
-        //                           medidaPorGramo = @MedidaPorGramo, 
-        //                           cantCalorias = @CantCalorias, aptoCeliacos = @AptoCeliacos, aptoDiabeticos = @AptoDiabeticos,
-        //                           aptoVegetarianos = @AptoVegetarianos, 
-        //                           aptoVeganos = @AptoVeganos, tipo = @Tipo, estacion = @Estacion
-        //                        WHERE id = @Id";
-        //    cmd.Parameters.Add(new SqlParameter("@Id", _Id));
-        //    cmd.Parameters.Add(new SqlParameter("@Nombre", _Nombre));
-        //    cmd.Parameters.Add(new SqlParameter("@Costo", _Costo));
-        //    cmd.Parameters.Add(new SqlParameter("@Medida", _Medida.ToString()));
-        //    cmd.Parameters.Add(new SqlParameter("@MedidaPromedio", _MedidaPromedio));
-        //    cmd.Parameters.Add(new SqlParameter("@MedidaPorGramo", _MedidaPorGramo));
-        //    cmd.Parameters.Add(new SqlParameter("@CantCalorias", _CantCaloriasPorMedida));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoCeliacos", (bool)_AptoCeliacos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoDiabeticos", (bool)_AptoDiabeticos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoVegetarianos", (bool)_AptoVegetarianos));
-        //    cmd.Parameters.Add(new SqlParameter("@AptoVeganos", (bool)_AptoVeganos));
-        //    cmd.Parameters.Add(new SqlParameter("@Tipo", (int)_Tipo));
-        //    cmd.Parameters.Add(new SqlParameter("@Estacion", (int)_Estacion));
-        //    cmd.Connection = cn;
-        //    try
-        //    {
-        //        ManejadorConexion.AbrirConexion(cn);
-        //        int filas = cmd.ExecuteNonQuery();
-        //        return filas == 1;
-        //    }
-        //    catch (SqlException ex)
-        //    {
-        //        System.Diagnostics.Debug.Assert(false, ex.Message);
-        //        return false;
-        //    }
-        //    finally
-        //    {
-        //        ManejadorConexion.CerrarConexion(cn);
-        //    }
-        //}
-
-        //public bool Borrar()
-        //{
-        //    return false;
-        //}
-
+        public int _IdTipo { set; get; }
+        [ForeignKey("_IdTipo")]
+        [JsonIgnore]
+        public TipoIngrediente _Tipo { get; set; }
 
 
     }
